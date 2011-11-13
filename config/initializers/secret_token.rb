0534601ca0f81e5c85b31965ad00dc1c644be56e
BazZy <bazzy.bazzy@gmail.com>
2011-11-13 14:57:15 +0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogotron::Application.config.secret_token = '75fa59800d980deebba4cabdcb271568b3965ca463add4a47f3b57e854e6268f5704700f7e8ad84e58d461cf4673a8bb1a7abd8b6bf279e101bd1e03cce3140d'
