f39a47528e27e6b09d6b4901e0915fd93b9fb16a
BazZy <bazzy.bazzy@gmail.com>
2011-11-15 11:54:03 +0400

    Подчистил лишние комменты и файлы

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,79 +3,40 @@
 class CommentsController < ApplicationController
   before_filter :find_comment, :only => [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.xml
   def index
     @comments = Comment.all
-
-    respond_to do |wants|
-      wants.html # index.html.erb
-      wants.xml  { render :xml => @comments }
-    end
   end
 
-  # GET /comments/1
-  # GET /comments/1.xml
   def show
-    respond_to do |wants|
-      wants.html # show.html.erb
-      wants.xml  { render :xml => @comment }
-    end
   end
 
-  # GET /comments/new
-  # GET /comments/new.xml
   def new
     @comment = Comment.new
-
-    respond_to do |wants|
-      wants.html # new.html.erb
-      wants.xml  { render :xml => @comment }
-    end
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
-  # POST /comments.xml
   def create
     params[:comment][:user] = User.find(params[:comment][:user])
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(params[:comment])
-    # params[:comment][:user] = User.find(params[:comment][:user])
-    # post = Post.find(params[:comment][:post])
-    # params[:comment][:post] = post
-    # @comment = Comment.new(params[:comment])
-    # puts params[:comment].inspect
     
     if @comment.save
       redirect_to @post, :notice => "Комментарий успешно создан."
     else
-      # puts @comment.errors.messages.inspect
-      # redirect_to post, :alert => "При создании комментария возникла проблема."
       redirect_to @post, :alert => "Проблема при создании комментария: " + @comment.errors.messages[:content].first.to_s
     end
   end
 
-  # PUT /comments/1
-  # PUT /comments/1.xml
   def update
-    respond_to do |wants|
-      if @comment.update_attributes(params[:comment])
-        flash[:notice] = 'Comment was successfully updated.'
-        wants.html { redirect_to(@comment) }
-        wants.xml  { head :ok }
-      else
-        wants.html { render :action => "edit" }
-        wants.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-      end
+    if @comment.update_attributes(params[:comment])
+      redirect_to @comment, :notice => "Комментарий был успешно обновлен"
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.xml
   def destroy
     @comment.destroy
     redirect_to :back, :notice => "Ваш комментарий был успешно удален."
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
   
-  validates :subject, :presence => true
+  validates :subject, :presence => {:message => "не может быть пустой"}
   validates :content, :presence => true
   
   scope :ordered, :order => "updated_at ASC"
